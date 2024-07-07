e3bfe76e214908faaf5ac7fb6ac29621ef0d76f3
armd482 <armd482@naver.com>
2024-07-07 11:34:05 +0900

    Docs: 주석 추가

--- src/components/Header/HeaderParts/SearchBox.tsx
+++ src/components/Header/HeaderParts/SearchBox.tsx
@@ -5,7 +5,6 @@ import { usePathname, useRouter } from 'next/navigation';
 import classNames from 'classnames/bind';
 
 import { SearchIcon } from '@/public/index';
-
 import { ROUTER } from '@/constants/route';
 import { useOutsideClick } from '@/hooks/useOutsideClick';
 import SearchHistory from './SearchHistory';
@@ -34,14 +33,16 @@ export default function SearchBox({ isBlack }: SearchBoxProps) {
   const [autoListType, setAutoListType] = useState<'history' | 'suggestion'>('history');
   const [focusIndex, setFocusIndex] = useState(-1);
 
-  const [keywordHistory, setKeywordHistory] = useState(['aaa', 'bbb', 'ccc', 'ddd']);
+  const [keywordHistory, setKeywordHistory] = useState<string[]>([]);
 
+  /* 외부 클릭 시 focus 해제(자동 완성 리스트 안 뜨도록 설정 => 클릭한 곳이 suggestRef 내부일 경우 제외) */
   useOutsideClick(inputRef, (e?: MouseEvent) => {
     if (e && suggestRef.current && !suggestRef.current.contains(e.target as Node)) {
       setIsFocus(false);
     }
   });
 
+  /* 헤더 검색버튼 눌렀을 때 검색 컴포넌트 뜨도록 설정(열 때는 isRender도 같이 true로 바로 바뀌도록 설정) */
   const handleClickButton = () => {
     setIsOpen((prev) => {
       if (!prev) {
@@ -52,18 +53,29 @@ export default function SearchBox({ isBlack }: SearchBoxProps) {
     });
   };
 
+  /* 
+    검색 컴포넌트 unmount 요청 시(isOpen값이 false로 바뀔 때)에 animation이 발생하고 다 끝난 후에 unmount되도록 하기 위한 코드. 
+    애니메이션이 종료된 후 isOpen이 false 일 때(사용자가 닫는 조건을 발동시켰을 때 - 외부 클릭 또는 헤더 검색 버튼을 누른 경우 ) unmount 되도록 설정
+    참고로 애니메이션은 처음에 isOpen이 true로 변경될 때 또는 false로 변경될 때 발동되며(slide-in, slide-out),
+    닫히는 경우, isOpen이 false로 변경되고, 애니메이션이 일어난 후, unmount되어야 함(isRender가 false)
+  */
   const handleAnimationEnd = () => {
     if (!isOpen) {
       setIsRender(false);
     }
   };
 
+  /* 
+    회색 배경 부분 클릭 했을 경우 검색 컴포넌트 닫히도록 설정
+    만약 헤더 클릭 했을 때도 닫히도록 하려면 그냥 useOutsideClick으로 하면 됨.
+  */
   const handleClickOutside = (e: React.MouseEvent<HTMLDivElement>) => {
     if (wrapperRef.current && !wrapperRef.current.contains(e.target as Node)) {
       handleClickButton();
     }
   };
 
+  /* input 값 변경 함수. 만약 값이 비어있을 경우, 최근 검색어 내역이 뜨도록 설정 */
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value);
     if (!e.target.value) {
@@ -77,6 +89,13 @@ export default function SearchBox({ isBlack }: SearchBoxProps) {
     setIsFocus(true);
   };
 
+  /* 
+    위, 아래 버튼 클릭 시에 해당 keyword focus되도록 설정 -> 최근 검색어 또는 검색어 제안의 값을 바로 가져올 수 있도록 설정
+    focusIndex를 키보드 입력을 했을 때 해당 선택 값으로 변경하고, searchKeyword 값을 해당 값으로 변경.
+    input의 selectionRange(커서 위치)를 해당 텍스트 가장 마지막으로 변경.
+    가장 마지막 또는 가장 처음에서 화살표 아래 또는 위를 누를 경우, focusIndex를 -1로 변경하고 keyword값 초기화/이전 값 불러오기
+    -1인 상태에서 누를 경우 가장 처음 또는 가장 마지막으로 focus 이동
+  */
   const handleInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     const value = e.key;
     if (value === 'ArrowDown') {
@@ -120,9 +139,14 @@ export default function SearchBox({ isBlack }: SearchBoxProps) {
     }
   };
 
+  /* 
+    input값 제출 => search페이지로 이동 -> 해당 input 값을 쿼리로 변경.
+    최근 검색어 저장
+  */
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsRender(false);
+    setKeywordHistory((prev) => [...prev.filter((keyword) => keyword !== searchKeyword), searchKeyword]);
     router.push(`${ROUTER.SEARCH}?keyword=${searchKeyword}`, { scroll: false });
   };
 
@@ -130,33 +154,45 @@ export default function SearchBox({ isBlack }: SearchBoxProps) {
     e.preventDefault();
   };
 
+  /* 
+    최근 검색어 클릭 함수
+    마찬가지로 해당 값으로 search페이지 이동.
+    최근 검색어 저장(순서 변경?)
+  */
   const handleClickHistoryKeyword = (index: number) => {
     setIsRender(false);
+    setKeywordHistory((prev) => [...prev.filter((keyword) => keyword !== searchKeyword), searchKeyword]);
     router.push(`${ROUTER.SEARCH}?keyword=${keywordHistory[index]}`, { scroll: false });
   };
 
+  /* 최근 검색어 요소 하나 삭제 함수 */
   const handleClickDeleteHistory = (index: number) => {
     setKeywordHistory((prev) => prev.filter((_, i) => i !== index));
   };
 
+  /* 최근 검색어 전체 삭제 함수 */
   const handleClickDeleteAllHistory = () => {
     setKeywordHistory([]);
   };
 
+  /* mouse hover시에 focusIndex 업데이트를 위한 함수 */
   const handleFocuseHistoryKeyword = (index: number) => {
     setFocusIndex(index);
   };
 
+  /* 페이지 이동 시 검색 컴포넌트 안보이도록 변경 */
   useEffect(() => {
     setIsRender(false);
   }, [pathName]);
 
+  /* 검색 컴포넌트가 열고 닫힐 때, 값이 초기화 되도록 설정 */
   useEffect(() => {
     setIsFocus(false);
     setSearchKeyword('');
     setAutoListType('history');
   }, [isRender]);
 
+  /* 아웃 포커스 되었을 때, 자동 완성창이 사라졌을 때 focusIndex 값도 초기화 되도록 설정 */
   useEffect(() => {
     setFocusIndex(-1);
   }, [isFocus, autoListType]);
