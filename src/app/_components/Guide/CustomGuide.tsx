c58041c6cbdc839e3cc4d35d7253a8e609087a44
Young2un <popduddms@gmail.com>
2024-07-10 12:54:57 +0900

    Style: 메인 css 아직 이상하지만 일단 커밋

--- src/app/_components/Guide/CustomGuide.tsx
+++ src/app/_components/Guide/CustomGuide.tsx
@@ -1,9 +1,8 @@
+import { Button } from '@/components';
 import { ROUTER } from '@/constants/route';
 import { keydeukImg } from '@/public/index';
 import classNames from 'classnames/bind';
 import Image from 'next/image';
-import Link from 'next/link';
-import AnimatedSection from './AnimatedSection';
 import styles from './CustomGuide.module.scss';
 import Guide from './Guide';
 
@@ -25,13 +24,13 @@ const GUIDE = [
   {
     step: 3,
     title: '키캡 선택',
-    desc: ['단일 색상인 키보드가 지겹지 않으셨나요?', '키득에선 다양한 색상으로 원하는 키캡을 커스텀 할 수 있어요'],
+    desc: ['단일 색상인 키보드가 지겹지 않으셨나요?', '다양한 색상으로 원하는 키캡을 커스텀 할 수 있어요'],
     src: '/images/guide3.png',
   },
   {
     step: 4,
     title: '장바구니',
-    desc: ['배열부터 키캡까지 내 취향을 담은 커스텀 키보드를 구입해보세요'],
+    desc: ['배열부터 키캡까지 내 취향을 담은', '커스텀 키보드를 구입해보세요'],
     src: '/images/guide4.png',
   },
 ];
@@ -40,25 +39,32 @@ export default function CustomGuide() {
   return (
     <section className={cn('custom-guide')}>
       <Image src={keydeukImg} width={145} height={145} alt='키득이미지' />
-      <h1 className={cn('title')}>커스텀 용어가 어렵다면 키득이 도와줄게요</h1>
-      <p className={cn('sub-title')}>3D 시뮬레이션으로 원하는 느낌 그대로 재현 할 수 있어요</p>
+      <h1 className={cn('title')} data-aos='fade-up'>
+        커스텀 용어가 어렵다면 키득이 도와줄게요
+      </h1>
+      <p className={cn('sub-title')} data-aos='fade-up' data-aos-once>
+        3D 시뮬레이션으로 원하는 느낌 그대로 재현 할 수 있어요
+      </p>
       <ul className={cn('guide-list')}>
         {GUIDE.map(({ step, title, desc, src }) => (
-          <AnimatedSection key={step}>
-            <Guide step={step} title={title} desc={desc} src={src} />
-          </AnimatedSection>
+          <Guide key={step} step={step} title={title} desc={desc} src={src} />
         ))}
       </ul>
-      <AnimatedSection>
-        <h1 className={cn('title')}>키보드 득템할 준비가 되었다면?</h1>
-      </AnimatedSection>
-      <AnimatedSection>
-        <Link href={ROUTER.CUSTOM_KEYBOARD} className={cn('button-area')}>
-          <button type='button' className={cn('font-24')}>
-            키보드 커스텀 하러 가기
-          </button>
-        </Link>
-      </AnimatedSection>
+      <h1 className={cn('title')} data-aos='fade-up' data-aos-once>
+        키보드 득템할 준비가 되었다면?
+      </h1>
+
+      <Button
+        as='a'
+        href={ROUTER.CUSTOM_KEYBOARD}
+        fontSize={24}
+        width={320}
+        data-aos='fade-up'
+        data-aos-once
+        className={cn('button-area')}
+      >
+        키보드 커스텀 하러 가기
+      </Button>
     </section>
   );
 }
