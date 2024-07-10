8447559635d6899c72301dbe745f0d6ea7839765
Young2un <popduddms@gmail.com>
2024-07-10 12:49:27 +0900

    Style: 사소한 수정

--- src/app/_components/Banner/Banner.tsx
+++ src/app/_components/Banner/Banner.tsx
@@ -9,25 +9,28 @@ const cn = classNames.bind(styles);
 
 export default function Banner() {
   return (
-    <div className={cn('container')}>
-      <Link href={ROUTER.CUSTOM_KEYBOARD} className={cn('banner')}>
-        <Image
-          src={banner1Img}
-          fill
-          alt='배너1'
-          priority
-          sizes='(max-width: 600px) 480px, (max-width: 1200px) 800px, 1200px'
-        />
-      </Link>
-      <Link href={ROUTER.COMMUNITY} className={cn('banner')}>
-        <Image
-          src={banner2Img}
-          fill
-          alt='배너2'
-          priority
-          sizes='(max-width: 600px) 480px, (max-width: 1200px) 800px, 1200px'
-        />
-      </Link>
-    </div>
+    <>
+      <h1 className={cn('title')}>키득 MORE</h1>
+      <div className={cn('container')}>
+        <Link href={ROUTER.CUSTOM_KEYBOARD} className={cn('banner')}>
+          <Image
+            src={banner1Img}
+            fill
+            alt='배너1'
+            priority
+            sizes='(max-width: 600px) 480px, (max-width: 1200px) 800px, 1200px'
+          />
+        </Link>
+        <Link href={ROUTER.COMMUNITY} className={cn('banner')}>
+          <Image
+            src={banner2Img}
+            fill
+            alt='배너2'
+            priority
+            sizes='(max-width: 600px) 480px, (max-width: 1200px) 800px, 1200px'
+          />
+        </Link>
+      </div>
+    </>
   );
 }
