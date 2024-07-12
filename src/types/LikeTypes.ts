571acefdb22bfa2402ec1b3a4aa3efee90cf0a8a
Young2un <popduddms@gmail.com>
2024-07-12 11:18:45 +0900

    Style: 필요없는 주석제거

--- src/types/LikeTypes.ts
+++ src/types/LikeTypes.ts
@@ -1,15 +1,9 @@
 export interface ProductLike {
-  productId: number; // 제품 ID
-  productImg: string; // 제품 이미지 URL
-  productName: string; // 제품 이름
-  price: number; // 제품 가격
+  productId: number;
+  productImg: string;
+  productName: string;
+  price: number;
 }
-
-// export interface GetProductLikesResponse {
-//   status: string; // 응답 상태
-//   message: string | null; // 응답 메시지
-//   data: ProductLike[]; // 좋아요한 제품 리스트
-// }
 
 export interface GetProductLikesParams {
   page?: string;
