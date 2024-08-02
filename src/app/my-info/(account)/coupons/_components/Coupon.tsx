625c62a61b9d3b2b2e175dff0c50fdd2efe3fbac
minjeong9919 <minjeong9919@naver.com>
2024-08-02 11:03:40 +0900

    Feat: 쿠폰 컴포넌트 구현

--- src/app/my-info/(account)/coupons/_components/Coupon.tsx
+++ src/app/my-info/(account)/coupons/_components/Coupon.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import classNames from 'classnames/bind';
-import { VerticalTripleDotIcon, KeydeukBlueIcon } from '@/public/index';
+import { VerticalTripleDotIcon, KeydeukBlueIcon, ChevronIcon } from '@/public/index';
 import { useState } from 'react';
+import { formatDateToString } from '@/libs/formatDateToString';
 
 import styles from './Coupon.module.scss';
 
@@ -19,23 +20,51 @@ const COUPON_INFO = {
 
 export default function Coupon() {
   const [isClickedTripleDot, setIsClickedTripleDot] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-  const handleClickTripleDot = () => {
+  const handleClickIcon = () => {
+    setIsAnimating(true);
     setIsClickedTripleDot(!isClickedTripleDot);
+
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 300);
   };
 
+  const expiredAt = formatDateToString(new Date(COUPON_INFO.expiredAt));
+
   return (
-    <div className={cn('container')}>
-      <div className={cn('info-wrapper')}>
-        <h1 className={cn('discount-rate')}>{COUPON_INFO.price}원</h1>
-        <h3 className={cn('name')}>{COUPON_INFO.name}</h3>
-        <p>최소주문금액: {COUPON_INFO.minPrice}</p>
-        <p>사용기간: {COUPON_INFO.expiredAt}</p>
-      </div>
-      <div className={cn('logo-wrapper')}>
-        <VerticalTripleDotIcon className={cn('triple-dot')} />
-        <KeydeukBlueIcon width={60} height={70} onClick={handleClickTripleDot} />
-      </div>
+    <div className={cn('container', { 'slit-in-vertical': isAnimating })}>
+      {!isClickedTripleDot ? (
+        <>
+          <div className={cn('info-wrapper')}>
+            <h1 className={cn('discount-rate')}>{COUPON_INFO.price}원</h1>
+            <h3 className={cn('name')}>{COUPON_INFO.name}</h3>
+            <p className={cn('coupon-info')}>최소주문금액: {COUPON_INFO.minPrice}</p>
+            <p className={cn('coupon-info')}>사용기간: {expiredAt}</p>
+          </div>
+          <VerticalTripleDotIcon className={cn('icon', 'triple-dot')} fill='#c6c6c6' onClick={handleClickIcon} />
+          <div className={cn('logo-wrapper')}>
+            <KeydeukBlueIcon width={80} height={90} />
+          </div>
+        </>
+      ) : (
+        <>
+          <ChevronIcon
+            width={20}
+            height={10}
+            className={cn('icon', 'chevron')}
+            onClick={handleClickIcon}
+            fill='#a5a5a5'
+          />
+          <ul className={cn('explanation-list')}>
+            <li>최소 {COUPON_INFO.minPrice} 원 이상 주문 시 쿠폰 적용이 가능합니다.</li>
+            <li>쿠폰은 다른 계정으로 양도하실 수 없습니다. </li>
+            <li>이 쿠폰은 2024년 8월 1일까지 유효합니다. </li>
+            <li>이 쿠폰은 한 계정당 1회만 사용할 수 있습니다.</li>
+          </ul>
+        </>
+      )}
     </div>
   );
 }
