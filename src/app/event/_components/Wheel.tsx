1d9e9f35858f44d21bc8d37e6b822d8cbef4aa61
Young2un <popduddms@gmail.com>
2024-08-14 02:12:33 +0900

    Style: 대소문자 이슈,,

--- src/app/event/_components/Wheel.tsx
+++ src/app/event/_components/Wheel.tsx
@@ -7,7 +7,7 @@ import { Dialog } from '@/components';
 import SignInModal from '@/components/SignInModal/SignInModal';
 import { ANIMATION_DURATION, CONFETTI_NUMBER, EMOJI_SIZE, MIN_PRICE_MULTIPLIER, REWARDS } from '@/constants/event';
 import { useCreateCouponMutation } from '@/hooks/useCreateCouponMutation';
-import { calculateRotation, getRandomIndex, getTodayDateString, hasRouletteCoupon } from '@/libs/wheelutils';
+import { calculateRotation, getRandomIndex, getTodayDateString, hasRouletteCoupon } from '@/libs/wheelUtils';
 import { CouponDataType, CouponResponse } from '@/types/couponType';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
