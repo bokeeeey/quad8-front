0bf3c92a2198237d01a25fcd86425cdd10fdaed2
armd482 <armd482@naver.com>
2024-08-16 15:14:51 +0900

    Feat: interceptor 구현

---
+++ src/api/interceptor/interceptor.ts
@@ -0,0 +1,87 @@
+import { getCookie } from '@/libs/manageCookie';
+import { updateToken } from './updateToken';
+
+interface ResponseAPIType<T> {
+  data: T;
+  message: string;
+  status: string;
+}
+const requestAPI = async <T>(baseURL: string, url: string, option?: RequestInit): Promise<ResponseAPIType<T>> => {
+  const accessToken = await getCookie('accessToken');
+
+  if (!accessToken) {
+    try {
+      const response = await fetch(baseURL + url, {
+        ...option,
+        headers: {
+          'Content-Type': 'application/json',
+          ...option?.headers,
+        },
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  try {
+    const response = await fetch(baseURL + url, {
+      ...option,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+        ...option?.headers,
+      },
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      const refreshToken = await getCookie('refreshToken');
+      if (response.status === 401 && accessToken && refreshToken) {
+        await updateToken();
+        const result: ResponseAPIType<T> = await requestAPI(baseURL, url, option);
+        return result;
+      }
+      throw new Error(data.message);
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+class Interceptor {
+  baseURL: string;
+
+  constructor(baseURL: string) {
+    this.baseURL = baseURL;
+  }
+
+  async get<T>(url: string, option?: RequestInit) {
+    const data = await requestAPI<T>(this.baseURL, url, { ...option, method: 'GET' });
+    return data;
+  }
+
+  async post<T>(url: string, option?: RequestInit) {
+    const data = await requestAPI<T>(this.baseURL, url, { ...option, method: 'POST' });
+    return data;
+  }
+
+  async put<T>(url: string, option?: RequestInit) {
+    const data = await requestAPI<T>(this.baseURL, url, { ...option, method: 'PUT' });
+    return data;
+  }
+
+  async delete<T>(url: string, option?: RequestInit) {
+    const data = await requestAPI<T>(this.baseURL, url, { ...option, method: 'DELETE' });
+    return data;
+  }
+}
+
+export const baseAPI = new Interceptor(process.env.NEXT_PUBLIC_KEYDEUK_API_BASE_URL as string);
---
+++ src/api/interceptor/updateToken.ts
@@ -0,0 +1,32 @@
+'use client';
+
+import { deleteCookie, getCookie, setCookie } from '@/libs/manageCookie';
+
+const BASE_URL = process.env.NEXT_PUBLIC_KEYDEUK_API_BASE_URL;
+
+export const updateToken = async () => {
+  const accessToken = await getCookie('accessToken');
+  const refreshToken = await getCookie('refreshToken');
+
+  try {
+    const response = await fetch(`${BASE_URL}/api/v1/reissue`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ accessToken, refreshToken }),
+    });
+
+    const { data: token, message } = await response.json();
+
+    if (!response.ok) {
+      throw new Error(message);
+    }
+
+    setCookie('accessToken', token);
+  } catch (error) {
+    deleteCookie('accessToken');
+    deleteCookie('refreshToken');
+    throw error;
+  }
+};
