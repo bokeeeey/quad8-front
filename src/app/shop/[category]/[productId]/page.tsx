02d4af8b5690f6d046177c21a10970ded12ed8e1
김보경 <rlaqhrud6263@naver.com>
2024-12-13 02:14:28 +0900

    [feat] product 페이지 metadata 설정 (#200)
    * Feat: 악성봇 redirects 추가
    
    * Fix: shop 페이지 정상화
    
    * Feat: product 페이지 metadata 설정
    
    * Fix: metadata 수정
    
    * Fix: metadata 수정

--- src/app/shop/[category]/[productId]/page.tsx
+++ src/app/shop/[category]/[productId]/page.tsx
@@ -1,11 +1,29 @@
+import { getQueryClient } from '@/libs/client';
+import { Metadata } from 'next';
+import { fetchProductQuery } from '@/libs/prefetchers';
 import ProductDetail from './_components/Product/ProductDetail';
 import DetailTab from './_components/TabContents/DetailTab';
+import { createProductMetadata } from './_utils/metadata';
 
 interface ProductDetailParams {
   params: {
     [param: string]: string;
   };
 }
+
+export const generateMetadata = async ({ params }: ProductDetailParams): Promise<Metadata> => {
+  const { productId } = params;
+  const queryClient = getQueryClient();
+  const product = await fetchProductQuery(queryClient, productId);
+
+  if (!product) {
+    return {
+      title: '상품 정보를 불러올 수 없습니다.',
+    };
+  }
+
+  return createProductMetadata({ product });
+};
 
 export default async function ProductDetailPage({ params }: ProductDetailParams) {
   const { productId } = params;
